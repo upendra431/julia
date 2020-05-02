294fd17d7b3fe44e2338a7351373c3e2609d3ca1
upendra431 <uppu431@gmail.com>
2020-05-02 18:38:42 +0530

    Strings

---
+++ Strings/StringStart.jl
@@ -0,0 +1,28 @@
+myGrade='A'
+println(Int(myGrade))
+
+name="Upendra"
+
+println(length(name))
+
+
+nameIn="""Hello
+My name
+
+Is"""
+
+println(nameIn)
+
+println(nameIn[1])
+println(nameIn[end])
+println(nameIn[end-1])
+
+println(nameIn[2:end-1])
+
+w1="Ram"
+w2="babu"
+println(w1 * " " *w2)
+
+a=10
+b=20
+println("the result of $a + $b is $(a+b)")
