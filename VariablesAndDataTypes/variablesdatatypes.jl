8ff401f35e16ce260706db7fd01592d7890a9d7b
upendra431 <uppu431@gmail.com>
2020-05-02 17:27:15 +0530

    VariablesAndDataTypes

---
+++ VariablesAndDataTypes/variablesdatatypes.jl
@@ -0,0 +1,17 @@
+first=1
+second=2.0
+stringvar="This is String"
+intial='J'
+
+boolval=true
+
+println(first)
+println(second)
+println(stringvar)
+println(intial)
+println(boolval)
+
+
+const  name="Ram"
+name="sam"
+println(name)
